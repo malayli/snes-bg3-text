2600c860a7987f4871210fdaf867d02b229063f2
Malik Alayli <malik.alayli@gmail.com>
2022-01-02 00:46:52 +0100

    Update

--- src/main.c
+++ src/main.c
@@ -1,19 +1,20 @@
 #include <snes.h>
 
 // ROM
-#define bg3TileMapAddress 0x1000+2048 // so bg1 tile map can be on 0x1024 and bg2 tile map on 0x1024 + 1024
-#define bg3TileSetAddress 0x4000 // so bg1 tile set can be on 0x2000 and bg2 tile set on 0x3000
-
-extern char snesfontbg3_tiles, snesfontbg3_tiles_end;
-extern char snesfontbg3_map, snesfontbg3_map_end;
-extern char snesfontbg3_pal;
-
-// RAM
 
 // BG1
 extern char patterns, patterns_end;
 extern char palette, palette_end;
 extern char map, map_end;
+
+// BG3
+#define bg3TileMapAddress 0x1000+2048 // so bg1 tile map can be on 0x1024 and bg2 tile map on 0x1024 + 1024
+#define bg3TileSetAddress 0x4000 // so bg1 tile set can be on 0x2000 and bg2 tile set on 0x3000
+extern char snesfontbg3_tiles, snesfontbg3_tiles_end;
+extern char snesfontbg3_map, snesfontbg3_map_end;
+extern char snesfontbg3_pal;
+
+// RAM
 
 // BG3
 bool refreshBg3Text = FALSE;
